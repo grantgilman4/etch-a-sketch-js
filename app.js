2f030bda8dba4e46604b7981b0d419f6e12457c7
Andrew Porter <andrewrporter@github.com>
2023-02-02 19:20:32 -0500

    Peer Review
    * Refactor
    * Use css style

--- app.js
+++ app.js
@@ -1,71 +1,59 @@
-const gridContainer = document.getElementById('gridContainer');
-gridContainer.style.display = 'grid';
-const buttons = document.getElementById('buttons');
-const sizingButton = document.createElement('button');
+const gridContainer = document.getElementById("gridContainer");
+const buttons = document.getElementById("buttons");
+
+const sizingButton = document.createElement("button");
 sizingButton.textContent = "Brush Size";
 buttons.appendChild(sizingButton);
-const rgbButton = document.createElement('button');
+const rgbButton = document.createElement("button");
 rgbButton.textContent = "Rainbow Mode";
 buttons.appendChild(rgbButton);
-const blackButton = document.createElement('button');
+const blackButton = document.createElement("button");
 blackButton.textContent = "Black";
 buttons.appendChild(blackButton);
-let lineColor = 'black'
-let hasBeenClicked = false;
 
-function makeGrid(rowNum, colNum) {
-    
-    for (i = 0; i < (rowNum * colNum); i++) {
-        let gridCell = document.createElement('div');
-        gridCell.setAttribute('id', 'gridCell');
-        gridContainer.appendChild(gridCell);
-    };
-    gridContainer.style.gridTemplateColumns = `repeat(${colNum}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${rowNum}, 1fr)`;
-    gridContainer.style.width = '50vw';
-    gridContainer.style.height = '50vw';
-};  
+let rgbHasBeenClicked = false;
+let pixelSize = 32;
 
-makeGrid(16, 16);
-
-    sizingButton.addEventListener('click', () => {
-        let pixelSize = prompt('What pixel size would you like to draw with?', '32');
-        const gridItems = gridContainer.querySelectorAll('#gridCell');
-        gridItems.forEach((gridCell) => {
-            gridCell.remove();
-        });
-        makeGrid(pixelSize, pixelSize); 
-        draw(lineColor); 
-    });
-
-    rgbButton.addEventListener('click', () => {
-        hasBeenClicked = true;
-        const gridItems = gridContainer.querySelectorAll('#gridCell');
-        gridItems.forEach((gridCell) => {
-            gridCell.style.backgroundColor = 'white';
-            draw(lineColor);
-        });
-    });
-
-    blackButton.addEventListener('click', () => {
-        hasBeenClicked = false;
-        const gridItems = gridContainer.querySelectorAll('#gridCell');
-        gridItems.forEach((gridCell) => {
-            gridCell.style.backgroundColor = 'white';
-        });
-        draw('black');
-    });
-
-function draw(lineColor) {
-    const gridItems = gridContainer.querySelectorAll('#gridCell');
-    gridItems.forEach((gridCell) => {
-    gridCell.addEventListener('mouseover', (e) =>  {
-        if (hasBeenClicked) {
-            lineColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
-        };
-        e.target.style.backgroundColor = lineColor;
-    });
-});
+const setLineColor = (e) => {
+  let lineColor = "black";
+  if (rgbHasBeenClicked) {
+    lineColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+  }
+  e.target.style.backgroundColor = lineColor;
 };
 
-draw(lineColor);
+function makeGrid(rowNum, colNum) {
+  for (i = 0; i < rowNum * colNum; i++) {
+    let gridCell = document.createElement("div");
+    gridCell.setAttribute("id", "gridCell");
+    gridCell.addEventListener("mouseover", (e) => {
+      setLineColor(e);
+    });
+    gridContainer.appendChild(gridCell);
+  }
+  gridContainer.style.gridTemplateColumns = `repeat(${colNum}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${rowNum}, 1fr)`;
+  gridContainer.style.width = "50vw";
+  gridContainer.style.height = "50vw";
+}
+
+sizingButton.addEventListener("click", () => {
+  pixelSize = prompt("What pixel size would you like to draw with?", "32");
+  const gridItems = gridContainer.querySelectorAll("#gridCell");
+  gridItems.forEach((gridCell) => {
+    gridCell.remove();
+  });
+  makeGrid(pixelSize, pixelSize);
+});
+
+rgbButton.addEventListener("click", () => {
+  rgbHasBeenClicked = true;
+  makeGrid(pixelSize, pixelSize);
+});
+
+blackButton.addEventListener("click", () => {
+  rgbHasBeenClicked = false;
+  makeGrid(pixelSize, pixelSize);
+});
+
+makeGrid(pixelSize, pixelSize);
